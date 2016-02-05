f1aad19d7561512d16f12b3213d8f629b98cb362
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-02-05 19:53:54 +0900

    adding authentication methods

--- app.js
+++ app.js
@@ -17,3 +17,27 @@ angular.module("yourApp").config(function ($routeProvider) {
   });
 
 });
+
+angular.moduel("yourApp").run(function ($rootScope, $location, AuthenticationService) {
+
+//enumerate routes that don't need authentication
+var routesThatDontRequireAuth = ['/login'];
+
+//check if current location matches route
+var routeClean = function(route) {
+  return _.find(routesThatDontRequireAuth, 
+    function (noAuthRoute) {
+      return _.str.startsWith(route, noAuthRoute);
+    });
+};
+
+  $rootScope.$on('$routeChangeStart', function (event, next, current) {
+    //if route requires auth and user is not logged in 
+    if (!routeClean($locaiton.url()) && !AuthenticationService.isLoggedIn()) {
+      //redirect back to login
+      $locaiton.path('/login');
+    }
+
+  });
+
+});
